9dcd26a85d84bbdfa882d40ace25bac6b18a2bda
ZhangMingZhao1 <1104272319@qq.com>
2021-07-04 21:56:18 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="container"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import LogicFlow from '@logicflow/core';
+import { BpmnElement } from '@logicflow/extension';
+import '@logicflow/core/dist/style/index.css';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+  },
+  data() {
+    return {
+      myData: {
+        // 节点
+        nodes: [
+          {
+            id: 21,
+            type: 'rect',
+            x: 100,
+            y: 200,
+            text: {
+              value: '矩形节点',
+              x: 100,
+              y: 200
+            }
+          },
+          {
+            id: 50,
+            type: 'circle',
+            x: 300,
+            y: 400,
+            text: {
+              value: '圆形节点',
+              x: 300,
+              y: 400
+            }
+          }
+        ],
+        // 边
+        edges: [
+          {
+            type: 'polyline',
+            sourceNodeId: 50,
+            targetNodeId: 21
+          }
+        ]
+      }
+    };
+  },
+  mounted(){
+    this.$_init();
+  },
+  methods: {
+    $_init() {
+      // 渲染画布
+      const lf = new LogicFlow({
+        container: document.querySelector('#container'),
+        stopScrollGraph: true,
+        stopZoomGraph: true,
+        width: 500,
+        height: 500,
+        grid: {
+          type: 'dot',
+          size: 20
+        }
+      });
+
+      lf.render(this.myData);
+
+      LogicFlow.use(BpmnElement);
+    }
+  },
+ 
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* #container {
+  height: 100px;
+  width: 100px;
+} */
 </style>
